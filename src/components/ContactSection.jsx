a335ea748e85f266f122f009279ce6d54b8c4d06
mfauzanfathan <mhmmdfauzan19@gmail.com>
2025-11-28 15:54:35 +0700

    update contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -69,10 +69,10 @@ export const ContactSection = () => {
                         <div className="pt-8">
                             <h4 className="font-medium mb-4">Connect With Me</h4>
                             <div className="flex space-x-4 justify-center">
-                                <a href="#" target="_blank">
+                                <a href="https://www.linkedin.com/in/muhammad-fauzan-fathan-49501522b/" target="_blank">
                                     <Linkedin />
                                 </a>
-                                <a href="#" target="_blank">
+                                <a href="https://www.instagram.com/mfauzanfathan_/" target="_blank">
                                     <Instagram />
                                 </a>
                             </div>
